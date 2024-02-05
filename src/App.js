348c1f36808dc8a379e3b478249e8b1d7216151f
Niklas Röttger <niklas.roettger@outlook.de>
2024-02-05 09:59:18 +0100

    add session code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from "react";
-import "./styles.css";
-
-function Animal({ emoji, name }) {
-  return (
-    <h2>
-      {emoji} {name}
-    </h2>
-  );
-}
+import React from 'react';
+import './styles.css';
+import Animal from './components/Animal';
 
 export default function App() {
   return (
     <main>
       <h1>Animal Shelter</h1>
       <p>Please adopt our lovely pets!</p>
-      <Animal emoji="🐕" name="Lucky" />
-      <Animal emoji="🐈" name="Mittens" />
-      <Animal emoji="🐢" name="Archibald" />
-      <Animal emoji="🐣" name="Chick Norris" />
+      <Animal emoji="🐕" name="Lucky" description={<p>Very cute dog</p>} />
+      <Animal emoji="🐈" name="Mittens" description={<p>A playful kitten</p>} />
+      <Animal
+        emoji="🐢"
+        name="Archibald"
+        description={<p>Very fast turtle</p>}
+      />
+      <Animal
+        emoji="🐣"
+        name="Chick Norris"
+        description={<p>A nested chick</p>}
+      />
     </main>
   );
 }
